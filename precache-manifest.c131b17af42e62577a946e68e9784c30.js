e9fdd2f1574fb9bbd51aebd781c807df0625677f
lcorb822 <48258398+lcorb822@users.noreply.github.com>
2020-04-06 23:18:02 -0600

    Updates

---
+++ precache-manifest.c131b17af42e62577a946e68e9784c30.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a254f8413d6fce4bb99da9ab2a43baad",
+    "url": "/Web3-Assignment2-Real/index.html"
+  },
+  {
+    "revision": "13e2de85308202d875fc",
+    "url": "/Web3-Assignment2-Real/static/css/2.5375aa1b.chunk.css"
+  },
+  {
+    "revision": "e7fe22101f34353d9d87",
+    "url": "/Web3-Assignment2-Real/static/css/main.330afbb0.chunk.css"
+  },
+  {
+    "revision": "13e2de85308202d875fc",
+    "url": "/Web3-Assignment2-Real/static/js/2.a529ef89.chunk.js"
+  },
+  {
+    "revision": "8c32977b1ed3abd86a00381b66ff2515",
+    "url": "/Web3-Assignment2-Real/static/js/2.a529ef89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7fe22101f34353d9d87",
+    "url": "/Web3-Assignment2-Real/static/js/main.9ad21add.chunk.js"
+  },
+  {
+    "revision": "d1d67d7cbeb785991bb4",
+    "url": "/Web3-Assignment2-Real/static/js/runtime-main.b8d8111d.js"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/Web3-Assignment2-Real/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  }
+]);
